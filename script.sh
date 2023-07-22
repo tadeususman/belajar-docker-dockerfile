9516d413146515cd64ff159b1d44eeb982cc58e7
Tadeus Usman <tadeus.udn@gmail.com>
2023-07-23 01:36:18 +0700

    feat: Added file doc

--- script.sh
+++ script.sh
@@ -58,6 +58,25 @@ docker container start expose
 docker container ls
 
 
+# ENV Instruction
+docker build -t tadeususman/env env
+
+docker image inspect tadeususman/env
+docker container create --name env --env APP_PORT=8090 -p 8090:8090 tadeususman/env
+docker container start env
+docker container ls
+
+# VOLUME Instruction
+docker build -t tadeususman/volume volume
+
+docker image inspect tadeususman/volume
+docker container create --name volume --volume APP_PORT=8080 -p 8080:8080 tadeususman/volume
+docker container start volume
+docker container ls
+docker container inspect volume
+
+# 4a429d53be0619d37bf300ffb64ec50778803dcfc9000a99d627bc3c1398bf10
+docker volume ls
 
 
 
